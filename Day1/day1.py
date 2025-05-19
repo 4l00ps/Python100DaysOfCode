2ed26b218142119c9ae34b8748b87121b96a73b8
4l00ps <matgits@icloud.com>
2025-05-19 10:09:39 +0300

    rename Main.py to day1.py

---
+++ Day1/day1.py
@@ -0,0 +1,22 @@
+myName = "Muhamamd Ali"
+welcomeNote = f"Hello {myName}"
+print(welcomeNote)
+
+#second way to print:
+print("Hello", myName)
+
+# third way to print:
+print("Hello " + myName)
+# fourth way to print:
+print("Hello {}".format(myName))
+# fifth way to print:
+print("Hello {0}".format(myName))
+# c style way of printing
+print("Hello %s" % myName)
+
+print("hello " + str(2))
+# str is used to convert the number to string
+# int is used to convert the string to number
+# float is used to convert the string to float
+# bool is used to convert the string to boolean
+
